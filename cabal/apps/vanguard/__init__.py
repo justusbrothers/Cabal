b3d095b3e05566eded0fcc1de6802e923cdad297
justusbros <b@justusbrothers.shop>
2026-09-04 20:17:23 +0000

    updated vanguard init

--- cabal/apps/vanguard/__init__.py
+++ cabal/apps/vanguard/__init__.py
@@ -1,6 +1,7 @@
 # /plugins/Cabal/cabal/apps/vanguard/__init__.py
 
-from .core import LookupPacksApiView, LookupSinceApiView, Vanguard
+from .core import Vanguard
+from .lookup import LookupPacksApiView, LookupSinceApiView
 
 __all__ = [
     "LookupPacksApiView",
